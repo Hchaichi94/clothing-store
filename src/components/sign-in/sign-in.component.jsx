23b3a7d1dff5dc863e8b4823a58379960604c591
akrem <46078485+Hchaichi94@users.noreply.github.com>
2020-03-21 00:25:20 +0100

    add sign-in email password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utlis";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utlis";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -12,10 +12,17 @@ class SignIn extends React.Component {
       password: ""
     };
   }
-  handleSubmit = event => {
+
+  handleSubmit = async event => {
     event.preventDefault();
-    console.log(this.state);
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: "", password: "" });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   handleChange = event => {
@@ -40,7 +47,7 @@ class SignIn extends React.Component {
           <FormInput
             name="password"
             type="password"
-            value={this.state.email}
+            value={this.state.password}
             onChange={this.handleChange}
             label="password"
             required
